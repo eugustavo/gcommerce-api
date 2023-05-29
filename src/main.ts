d5a059b39271c694db7cda451480981cf7466170
eugustavo <gustavo.ifto@gmail.com>
2023-05-29 15:01:42 -0300

    chore: config to deploy on fly.io

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors();
+
     // swagger
     const options = new DocumentBuilder()
         .setTitle('NestJS API')
@@ -15,6 +17,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(3000);
+    await app.listen(3333);
 }
 bootstrap();
